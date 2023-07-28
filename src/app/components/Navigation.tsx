fc66adafe8fb7c7994ebd564509a6719ab43dc38
diegoaguilop <diego.aguilar1811@gmail.com>
2023-07-28 15:56:29 +0200

    Creacion de la estrutura base

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import styles from './Navigation.module.css'
+
+const links = [
+  {
+    label: 'Home',
+    route: '/'
+  },
+  {
+    label: 'Login',
+    route: '/login'
+  },
+  {
+    label: 'Registro',
+    route: '/register'
+  },
+  {
+    label: 'Perfil',
+    route: '/profile'
+  }
+]
+
+export function Navigation () {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.navigation}>
+          {links.map(({ label, route }) => (
+            <li key={route}>
+              <Link href={route}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
